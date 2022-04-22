7ce94d62d71f4acce1d296c6e4a911d2f22c5874
Liam Toney <ldtoney@alaska.edu>
2022-04-22 13:30:30 -0800

    Plot all shot locations on node station map

--- imush_station_map.py
+++ imush_station_map.py
@@ -4,12 +4,17 @@ import os
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import pygmt
 from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 
 # Set PyGMT defaults
 pygmt.config(MAP_FRAME_TYPE='plain', FORMAT_GEO_MAP='D', FONT='10p')
+
+# Read in shot info
+df = pd.read_excel(Path(os.environ['NODAL_WORKING_DIR']) / 'iMUSH_shot_metadata.xlsx')
+df.dropna(inplace=True)
 
 # Grab "MSH Node Array"
 # http://ds.iris.edu/mda/1D/?starttime=2014-01-01T00:00:00&endtime=2014-12-31T23:59:59
@@ -22,20 +27,45 @@ lons = [sta.longitude for sta in net]
 lats = [sta.latitude for sta in net]
 codes = [int(sta.code) for sta in net]
 
-# A nice region
-REGION = [-122.34, -122.02, 46.08, 46.32]
+# Nice regions
+MAIN_REGION = [-122.42, -121.98, 46.06, 46.36]
+INSET_REGION = [-123.1, -121.3, 45.6, 46.8]
 
 # Plot
 fig = pygmt.Figure()
 shaded_relief = pygmt.grdgradient(
-    '@earth_relief_01s', region=REGION, azimuth=-45.0, normalize='t1+a0'
+    '@earth_relief_01s', region=MAIN_REGION, azimuth=-45.0, normalize='t1+a0'
 )
 pygmt.makecpt(cmap='gray', series=[-2, shaded_relief.values.max()])
-fig.grdimage(shaded_relief, cmap=True, projection='M4i', region=REGION, frame=False)
+fig.grdimage(
+    shaded_relief, cmap=True, projection='M4i', region=MAIN_REGION, frame=False
+)
 pygmt.makecpt(series=[np.min(codes), np.max(codes)], cmap='turbo', reverse=True)
-fig.plot(x=lons, y=lats, color=codes, style='c0.05i', cmap=True, pen='black')
-fig.basemap(map_scale='g-122.07/46.1+w5+f+l', frame=['WESN', 'a0.1f0.02'])
+fig.plot(x=lons, y=lats, color=codes, style='c0.05i', cmap=True, pen='black')  # Nodes
+fig.plot(x=df.Lon, y=df.Lat, style='s0.2i', color='black', pen='white')  # Shots
+fig.text(x=df.Lon, y=df.Lat, text=df.Shot, font='6p,white', justify='CM')  # Shot labels
+fig.basemap(map_scale='g-122.04/46.09+w5+f+l', frame=['WESN', 'a0.1f0.02'])
 fig.colorbar(frame='a200f100+l"Station code"')
+with fig.inset(position='JTR+w1.5i+o-0.5i/-1i', box='+gwhite+p1p'):
+    fig.plot(
+        x=lons,
+        y=lats,
+        color='black',
+        style='c0.01i',
+        region=INSET_REGION,
+        projection='M?',
+    )
+    in_main_map = (
+        (df.Lon > MAIN_REGION[0])
+        & (df.Lon < MAIN_REGION[1])
+        & (df.Lat > MAIN_REGION[2])
+        & (df.Lat < MAIN_REGION[3])
+    )
+    kwargs = dict(style='s0.07i', pen='black')
+    fig.plot(x=df[in_main_map].Lon, y=df[in_main_map].Lat, color='black', **kwargs)
+    fig.plot(x=df[~in_main_map].Lon, y=df[~in_main_map].Lat, color='white', **kwargs)
+    fig.basemap(map_scale='g-122.2/45.8+w50')
+
 fig.show()
 
 # fig.savefig(Path(os.environ['NODAL_WORKING_DIR']) / 'figures' / 'imush_station_map.png', dpi=600)
