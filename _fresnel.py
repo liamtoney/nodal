d9ec7494473cdf283a5e6c413e83f4d1b15b96f5
Liam Toney <ldtoney@alaska.edu>
2022-08-10 16:53:41 -0800

    Upgrade plotting code; good stopping point (generalizable!)

--- _fresnel.py
+++ _fresnel.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import utm
 import xarray as xr
+from matplotlib.colors import Normalize
 from pygmt.datasets import load_earth_relief
 from pyproj import CRS
 
 from utils import get_shots, get_stations
 
-M_PER_KM = 1000  # [m/km]
+M_PER_KM = 1000  # [m/km] CONSTANT
 
 MAIN_REGION = (-122.42, -121.98, 46.06, 46.36)  # From station map
 
@@ -87,12 +88,16 @@ for sta_x, sta_y in zip(sta_x_list, sta_y_list):
     dist = np.linalg.norm([shot_x - sta_x, shot_y - sta_y])
     n = int(np.ceil(dist / TARGET_SPACING))
 
-    # Make profile and add to list
+    # Make profile, clean up, and add to list
     profile = dem_utm.interp(
         x=xr.DataArray(np.linspace(shot_x, sta_x, n)),
         y=xr.DataArray(np.linspace(shot_y, sta_y, n)),
         method='linear',
     )
+    profile = profile.assign_coords(dim_0=_horiz_dist(profile))
+    profile = profile.rename(dim_0='distance').drop('spatial_ref')
+    profile.attrs = {}
+
     profiles.append(profile)
 
     # Print progress
@@ -101,17 +106,17 @@ for sta_x, sta_y in zip(sta_x_list, sta_y_list):
 
 print('Done')
 
-#%% Plot all profiles + paths
+#%% Calculate and plot all profiles + paths
 
-fig, (ax1, ax2, ax3) = plt.subplots(
-    nrows=3, sharex=True, sharey=True, figsize=(16.5, 7.5)
-)
+CMAP = 'viridis'  # Colormap for path length differences
+EQUAL_ASPECT = False  # Toggle for equal aspect ratio
 
-# Iterate over all profiles, calculating diffracted path and plotting profiles + paths
+# Iterate over all profiles, calculating paths
+ds_list = []
 for profile in profiles:
 
-    # Get horizontal distance; ensure numpy.ndarray type for function input
-    x = _horiz_dist(profile)
+    # Ensure numpy.ndarray type for function input
+    x = profile.distance.values
     y = profile.values
 
     # Compute DIRECT path
@@ -122,32 +127,77 @@ for profile in profiles:
     diff_path = diffracted_path(x, y)
     diff_path_len = np.linalg.norm([np.diff(x), np.diff(diff_path)], axis=0).sum()
 
-    # Plot profiles + paths, converting horizontal distance to km
-    common_kwargs = dict(linewidth=0.5, solid_capstyle='round')
-    ax1.plot(x / M_PER_KM, y, **common_kwargs)
-    ax2.plot(x / M_PER_KM, direct_path, **common_kwargs)
-    ax3.plot(x / M_PER_KM, diff_path, **common_kwargs)
-
-# Plot and label shot on each panel
-shot_coords = (x[0] / M_PER_KM, y[0])  # Using coords of last profile here (all same)
-for ax in ax1, ax2, ax3:
-    ax.scatter(
-        *shot_coords, s=250, marker='s', color='black', zorder=5, edgecolor='white'
+    # Make nice Dataset of all info
+    ds = xr.Dataset(
+        {
+            profile.name: profile,
+            'direct_path': ('distance', direct_path, dict(length=direct_path_len)),
+            'diffracted_path': ('distance', diff_path, dict(length=diff_path_len)),
+        },
+        attrs=dict(path_length_difference=diff_path_len - direct_path_len, units='m'),
     )
+    ds_list.append(ds)
+
+# Set up axes
+fig, axes = plt.subplot_mosaic(
+    [['a', 'cax'], ['b', 'cax'], ['c', 'cax']],
+    gridspec_kw=dict(width_ratios=(60, 1)),
+    figsize=(16.5, 7.5),
+)
+axes['a'].sharex(axes['b'])
+axes['b'].sharex(axes['c'])
+axes['a'].sharey(axes['b'])
+axes['b'].sharey(axes['c'])
+axes['a'].tick_params(labelbottom=False)
+axes['b'].tick_params(labelbottom=False)
+
+# Grab path length differences to set up colormap
+path_length_diffs = np.array([ds.path_length_difference for ds in ds_list])
+sm = plt.cm.ScalarMappable(
+    cmap=plt.cm.get_cmap(CMAP),
+    norm=Normalize(vmin=path_length_diffs.min(), vmax=path_length_diffs.max()),
+)
+
+# Plot all profiles + paths, sorted from smallest to largest path difference
+for i in path_length_diffs.argsort():
+
+    # Get the Dataset
+    ds = ds_list[i]
+
+    # Plot (onverting horizontal distance to km here)
+    color = sm.to_rgba(ds.path_length_difference)
+    common_kwargs = dict(linewidth=0.5, solid_capstyle='round')
+    axes['a'].plot(ds.distance / M_PER_KM, ds.elevation, c=color, **common_kwargs)
+    axes['b'].plot(ds.distance / M_PER_KM, ds.direct_path, c=color, **common_kwargs)
+    axes['c'].plot(ds.distance / M_PER_KM, ds.diffracted_path, c=color, **common_kwargs)
+
+# Plot and label shot on each panel; using coords of last profile here (all same)
+shot_coords = (ds.distance[0] / M_PER_KM, ds.elevation[0])
+for ax in axes['a'], axes['b'], axes['c']:
+    ax.scatter(*shot_coords, s=250, marker='s', color='black', zorder=5, ec='white')
     ax.text(*shot_coords, SHOT, color='white', va='center', ha='center', zorder=6)
 
 # Label panels
 loc = (0.993, 0.9)
-ax1.text(*loc, 'Elevation profiles', ha='right', transform=ax1.transAxes)
-ax2.text(*loc, 'Direct paths', ha='right', transform=ax2.transAxes)
-ax3.text(*loc, 'Shortest diffracted paths', ha='right', transform=ax3.transAxes)
+axes['a'].text(*loc, 'Elevation profiles', ha='right', transform=axes['a'].transAxes)
+axes['b'].text(*loc, 'Direct paths', ha='right', transform=axes['b'].transAxes)
+axes['c'].text(
+    *loc, 'Shortest diffracted paths', ha='right', transform=axes['c'].transAxes
+)
 
 # Final axis tweaks
-if False:
-    for ax in ax1, ax2, ax3:
+if EQUAL_ASPECT:
+    for ax in axes['a'], axes['b'], axes['c']:
         ax.set_aspect(1 / M_PER_KM)
-ax2.set_ylabel('Elevation (m)')  # Since this is the middle panel
-ax3.set_xlabel('Horizontal distance (km)')
+axes['b'].set_ylabel('Elevation (m)')  # Since this is the middle panel
+axes['c'].set_xlabel('Horizontal distance (km)')
+
+# Add overall colorbar
+fig.colorbar(
+    sm,
+    cax=axes['cax'],
+    label='Difference between shortest diffracted path and direct path (m)',
+)
 
 fig.tight_layout()
 fig.show()
