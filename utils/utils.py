fe00752879e45eb18e074271c517e4f19e26874c
Liam Toney <ldtoney@alaska.edu>
2022-08-23 11:54:52 -0800

    Plot "outside arrows" for shots outside map boundary

--- utils/utils.py
+++ utils/utils.py
@@ -19,6 +19,10 @@ V_P = 5000  # [m/s] P-wave speed
 FULL_REGION = (-123.1, -121.3, 45.6, 46.8)  # All 23 shots
 INNER_RING_REGION = (-122.42, -121.98, 46.06, 46.36)  # Inner ring of 8 shots
 
+# "Outside arrow" parameters
+BOUNDARY_PAD = 1  # [km] Padding between map boundary and arrow head
+ARROW_LENGTH = 3  # [km] Length of arrow
+
 # Code constants
 M_PER_KM = 1000  # [m/km] CONSTANT
 
@@ -160,12 +164,55 @@ def station_map(
         cmap=True,
         pen='black',
     )
-    fig.plot(
-        x=df_plot.lon, y=df_plot.lat, style='s0.2i', color='black', pen='white'
-    )  # Shots
-    fig.text(
-        x=df_plot.lon, y=df_plot.lat, text=df_plot.index, font='6p,white', justify='CM'
-    )  # Shot names
+
+    # Plot shots
+    if (
+        df_plot.shape[0] == 1
+        and not (
+            (df_plot.lon > region[0])
+            & (df_plot.lon < region[1])
+            & (df_plot.lat > region[2])
+            & (df_plot.lat < region[3])
+        ).to_numpy()
+    ):
+        # If only one shot is requested AND it's outside the map
+        tail_coords, head_coords, tail_shot_dist_km = _outside_arrow(
+            region,
+            sta_lons.mean(),
+            sta_lats.mean(),
+            df_plot.lon.squeeze(),
+            df_plot.lat.squeeze(),
+            BOUNDARY_PAD,
+            ARROW_LENGTH,
+        )
+        # Plot arrow
+        fig.plot(
+            x=tail_coords[0],
+            y=tail_coords[1],
+            style='v0.1i+e+gblack+a45+s',
+            pen='1p',
+            direction=[[head_coords[0]], [head_coords[1]]],
+        )
+        # Plot arrow distance text
+        fig.text(
+            x=tail_coords[0],
+            y=tail_coords[1],
+            text=f'{tail_shot_dist_km:.1f} km',
+            font='6p,Helvetica-Bold',
+            justify='LM',
+            offset='0.12i/0',
+            no_clip=True,
+        )
+        shot_x = tail_coords[0]
+        shot_y = tail_coords[1]
+    else:
+        shot_x = df_plot.lon
+        shot_y = df_plot.lat
+    # Plot shots
+    fig.plot(x=shot_x, y=shot_y, style='s0.2i', color='black', pen='white')
+    # Plot shot names
+    fig.text(x=shot_x, y=shot_y, text=df_plot.index, font='6p,white', justify='CM')
+
     # TODO: below fig.basemap() values optimized for the default region only!
     fig.basemap(map_scale='g-122.04/46.09+w5+f+l', frame=['WESN', 'a0.1f0.02'])
     fig.colorbar(frame=f'{cbar_tick_ints}+l"{cbar_label}"')
@@ -197,6 +244,97 @@ def station_map(
     fig.show()
 
     return fig
+
+
+def _outside_arrow(
+    region, lon1, lat1, lon2, lat2, boundary_pad_km, arrow_length_km, interval_km=0.1
+):
+    """Determine optimal arrow tail and head locations subject to placement constraints.
+
+    The user request an arrow length and the amount of padding between the arrow head
+    and the region boundary. The arrow points from (lon1, lat1), inside the map, to
+    (lon2, lat2), outside the map.
+
+    Args:
+        region (list): Region, in PyGMT format
+        lon1 (int or float): Longitude of inside point
+        lat1 (int or float): Latitude of inside point
+        lon2 (int or float): Longitude of outside point
+        lat2 (int or float): Latitude of outside point
+        boundary_pad_km (int or float): [km] Padding between arrow head and region
+            boundary (measured along the arrow's axis!)
+        arrow_length_km (int or float): [km] Length of arrow from tail to head
+        interval_km (int or float): [km] Spacing of intermediate points, used to
+            determine optimal arrow location - may need to decrease this for smaller
+            regions
+
+    Returns:
+        tuple: Tuple containing:
+        * tuple: (tail_lon, tail_lat)
+        * tuple: (head_lon, head_lat)
+        * float: [km] Distance from arrow tail to the outside point (lon2, lat2)
+    """
+
+    points = _get_points(lon1, lat1, lon2, lat2, interval_km)
+    in_region = (
+        (points.r > region[0])
+        & (points.r < region[1])
+        & (points.s > region[2])
+        & (points.s < region[3])
+    )
+    end_ind = min(-1, -int(round(boundary_pad_km / interval_km)))
+    start_ind = end_ind - int(round(arrow_length_km / interval_km))
+
+    return (
+        (points[in_region].iloc[start_ind].r, points[in_region].iloc[start_ind].s),
+        (points[in_region].iloc[end_ind].r, points[in_region].iloc[end_ind].s),
+        points.iloc[-1].p - points[in_region].iloc[start_ind].p,
+    )
+
+
+def _get_points(lon1, lat1, lon2, lat2, interval_km):
+    """Calculate intermediate longitudes, latitudes, and distances between two points.
+
+    Uses pyproj (WGS 84 ellipsoid) if it's installed, otherwise uses PyGMT (spherical).
+
+    Args:
+        lon1 (int or float): Longitude of first point
+        lat1 (int or float): Latitude of first point
+        lon2 (int or float): Longitude of second point
+        lat2 (int or float): Latitude of second point
+        interval_km (int or float): Desired spacing between intermediate points
+
+    Returns:
+        pandas.DataFrame: DataFrame of longitudes, latitudes, and distances in km
+    """
+
+    try:
+        from pyproj import Geod  # fmt: skip
+        print('pyproj found, using pyproj to calculate points')
+
+        geod = Geod(ellps='WGS84')
+        rs = geod.inv_intermediate(
+            lon1,
+            lat1,
+            lon2,
+            lat2,
+            del_s=interval_km * M_PER_KM,  # [m]
+            initial_idx=0,
+            terminus_idx=0,
+        )
+        p = geod.inv(
+            np.full(rs.npts, rs.lons[0]), np.full(rs.npts, rs.lats[0]), rs.lons, rs.lats
+        )[2]
+        points = pd.DataFrame(dict(r=rs.lons, s=rs.lats, p=np.array(p) / M_PER_KM))
+
+    except ImportError:
+        print('pyproj not found, using PyGMT to calculate points')
+
+        points = pygmt.project(
+            center=(lon1, lat1), endpoint=(lon2, lat2), generate=interval_km, unit=True
+        )
+
+    return points
 
 
 def _estimate_mask_distance(t_sep, c, v_p):
