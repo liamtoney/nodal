b0b50927fcf508c08f6885be7ca5b045e7fc5d26
Liam Toney <ldtoney@alaska.edu>
2022-10-14 17:03:59 -0800

    Compute and plot the distance from each node to the profile

--- fdprop/make_profile.py
+++ fdprop/make_profile.py
@@ -1,4 +1,6 @@
+import colorcet as cc
 import matplotlib.pyplot as plt
+import numpy as np
 from infresnel import calculate_paths
 from pyproj import CRS, Transformer
 
@@ -23,31 +25,61 @@ ds_list, dem = calculate_paths(
 )
 profile = ds_list[0].elevation
 
+# Convert station coordinates to UTM
+dem_crs = CRS(dem.rio.crs)
+proj = Transformer.from_crs(dem_crs.geodetic_crs, dem_crs)
+sta_x, sta_y = [], []
+for sta in get_stations()[0]:
+    x, y = proj.transform(sta.latitude, sta.longitude)
+    sta_x.append(x)
+    sta_y.append(y)
+
+# Find m and b in y = mx + b for profile
+m = (profile.y[-1] - profile.y[0]) / (profile.x[-1] - profile.x[0])
+b = profile.y[0] - m * profile.x[0]
+
+# Compute closest distance [m] to profile
+out_of_plane_dists = []
+for x, y in zip(sta_x, sta_y):
+    u2, v2 = x, (m * x) + b
+    u3, v3 = (y - b) / m, y
+    theta = np.arctan((v3 - v2) / (u2 - u3))
+    d = np.sin(theta) * (u2 - u3)
+    out_of_plane_dists.append(d)
+dist_lim = np.min([np.abs(np.min(out_of_plane_dists)), np.max(out_of_plane_dists)])
+
 # Plot map view
-fig, ax = plt.subplots()
-dem.plot.imshow(ax=ax)
+fig, ax = plt.subplots(figsize=(8, 7))
+dem.plot.imshow(ax=ax, cmap=cc.m_gray)
 ax.plot(
     [profile.x[0], profile.x[-1]], [profile.y[0], profile.y[-1]], color='tab:orange'
 )
 xlim, ylim = ax.get_xlim(), ax.get_ylim()
-dem_crs = CRS(dem.rio.crs)
-proj = Transformer.from_crs(dem_crs.geodetic_crs, dem_crs)
-for sta in get_stations()[0]:
-    ax.scatter(
-        *proj.transform(sta.latitude, sta.longitude),
-        s=10,
-        color='white',
-        edgecolor='black',
-    )
+sm = ax.scatter(
+    sta_x,
+    sta_y,
+    s=20,
+    c=out_of_plane_dists,
+    edgecolor='black',
+    linewidths=0.5,
+    vmin=-dist_lim,
+    vmax=dist_lim,
+    cmap=cc.m_CET_D13,
+)
+ax.ticklabel_format(style='plain')
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
 ax.set_aspect('equal')
+cbar = fig.colorbar(sm, label='Distance from profile (m)', orientation='horizontal')
+cbar.ax.set_xticklabels([f'{x:g}' for x in np.abs(cbar.ax.get_xticks())])
+fig.tight_layout()
 fig.show()
 
 # Plot profile view
 fig, ax = plt.subplots(figsize=(20, 2.5))
 profile.plot(x='distance', ax=ax, color='tab:orange')
 ax.set_aspect('equal')
+fig.tight_layout()
 fig.show()
 
 # Print info about the profile
