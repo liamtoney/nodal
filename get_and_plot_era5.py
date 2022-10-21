429a23afb149de561c18d5c39aca5d845e53003d
Liam Toney <ldtoney@alaska.edu>
2022-10-20 16:58:48 -0800

    Wrap all non-function things in a __name__ == '__main__' check (non-ideal!)

--- get_and_plot_era5.py
+++ get_and_plot_era5.py
@@ -26,9 +26,10 @@ SAVE = True  # Toggle saving PNG files
 df = get_shots()
 
 # Input checks
-assert len(sys.argv) == 2, 'Must provide exactly one argument!'
-SHOT = sys.argv[1]
-assert SHOT in df.index, 'Argument must be a valid shot name!'
+if __name__ == '__main__':
+    assert len(sys.argv) == 2, 'Must provide exactly one argument!'
+    SHOT = sys.argv[1]
+    assert SHOT in df.index, 'Argument must be a valid shot name!'
 
 
 def get_era5_u_v_z(year, month, day, hour, level, region=None):
@@ -62,136 +63,140 @@ def get_era5_u_v_z(year, month, day, hour, level, region=None):
     return ds
 
 
-#%% Get ERA5 profile for a given shot
+if __name__ == '__main__':
 
-# Get shot info
-shot = df.loc[SHOT]
-time = pd.Timestamp(shot.time.datetime).round('1h').to_pydatetime()  # Nearest hour!
+    #%% Get ERA5 profile for a given shot
 
-# Get region surrounding shot so we can find nearest neighbor
-# TODO: Interpolate to exact shot time and location?
-ds = get_era5_u_v_z(
-    time.year,
-    time.month,
-    time.day,
-    time.hour,
-    level=ERA5_PRESSURE_LEVELS,  # Up to around 50 km?
-    region=FULL_REGION,  # To encompass all shots
-)
+    # Get shot info
+    shot = df.loc[SHOT]
+    time = pd.Timestamp(shot.time.datetime).round('1h').to_pydatetime()  # Nearest hour!
 
-# Select nearest 1D profile
-ds_shot = ds.sel(latitude=shot.lat, longitude=shot.lon, method='nearest')
-ds_shot['alt_km'] = (ds_shot.z / 9.8) / 1000  # [km] Convert to geopotential height
-
-#%% Plot the profile (vertical view)
-
-MAX_ALT = 12  # [km]
-
-ds_plot = ds_shot.where(ds_shot.alt_km < MAX_ALT, drop=True)
-
-fig, ax = plt.subplots(figsize=(5, 9))
-for wind, dir in zip([ds_plot.u, ds_plot.v], [270, 180]):
-    ax.plot(
-        wind,
-        ds_plot.alt_km,
-        color=cc.m_CET_C6(plt.Normalize(0, 360)(dir)),  # TODO: Must match grid code!
-        label=wind.standard_name.replace('_', ' ').capitalize(),
+    # Get region surrounding shot so we can find nearest neighbor
+    # TODO: Interpolate to exact shot time and location?
+    ds = get_era5_u_v_z(
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        level=ERA5_PRESSURE_LEVELS,  # Up to around 50 km?
+        region=FULL_REGION,  # To encompass all shots
     )
-ax.axvline(
-    0,  # Show 0 m/s wind as vertical line
-    linestyle=':',
-    color=plt.rcParams['grid.color'],
-    linewidth=plt.rcParams['grid.linewidth'],
-    zorder=-5,
-)
-ax.axhline(
-    shot.elev_m / 1000,  # Horizontal line at shot elevation
-    linestyle='--',
-    color='black',
-    linewidth=plt.rcParams['axes.linewidth'],
-    zorder=-5,
-    label='Shot elevation',
-)
-ax.set_xlabel('Wind speed (m/s)')
-ax.set_ylabel('Altitude (km)')
-title_str = (
-    rf'$\bf{{Shot~{shot.name}}}$'
-    + '\nModel time: '
-    + UTCDateTime(str(ds_plot.time.values)).strftime('%Y-%m-%d %H:%M')
-    + f'\nProfile location: ({ds_plot.latitude:.2f}°, {ds_plot.longitude:.2f}°)'.replace(
-        '-', '–'
+
+    # Select nearest 1D profile
+    ds_shot = ds.sel(latitude=shot.lat, longitude=shot.lon, method='nearest')
+    ds_shot['alt_km'] = (ds_shot.z / 9.8) / 1000  # [km] Convert to geopotential height
+
+    #%% Plot the profile (vertical view)
+
+    MAX_ALT = 12  # [km]
+
+    ds_plot = ds_shot.where(ds_shot.alt_km < MAX_ALT, drop=True)
+
+    fig, ax = plt.subplots(figsize=(5, 9))
+    for wind, dir in zip([ds_plot.u, ds_plot.v], [270, 180]):
+        ax.plot(
+            wind,
+            ds_plot.alt_km,
+            color=cc.m_CET_C6(
+                plt.Normalize(0, 360)(dir)
+            ),  # TODO: Must match grid code!
+            label=wind.standard_name.replace('_', ' ').capitalize(),
+        )
+    ax.axvline(
+        0,  # Show 0 m/s wind as vertical line
+        linestyle=':',
+        color=plt.rcParams['grid.color'],
+        linewidth=plt.rcParams['grid.linewidth'],
+        zorder=-5,
     )
-)
-ax.set_title(title_str)
-ax.set_ylim(0, MAX_ALT)
-ax.legend()
-fig.show()
-
-#%% Plot the profile (map view) [ABOVE CELL MUST BE RUN FIRST]
-
-fig, (ax, cax) = plt.subplots(
-    ncols=2, gridspec_kw=dict(width_ratios=[40, 1]), figsize=(8, 8)
-)
-
-# Form colormap
-cmap = cc.m_rainbow_r.copy()
-under_color = 'tab:gray'
-cmap.set_under(under_color)
-
-sm = ax.quiver(
-    np.zeros(ds_plot.alt_km.size),
-    np.zeros(ds_plot.alt_km.size),
-    ds_plot.u,
-    ds_plot.v,
-    ds_plot.alt_km,  # We're coloring the arrows by altitude
-    cmap=cmap,
-    angles='xy',
-    scale_units='xy',
-    scale=1,
-    width=0.005,
-    clim=(shot.elev_m / 1000, MAX_ALT),  # Start colormap at shot elevation
-)
-radius = 30  # Hard-coded for the speeds we see here!
-ax.set_xlim(-radius, radius)
-ax.set_ylim(-radius, radius)
-ax.set_aspect('equal')
-reference_speed = 10  # [m/s]  # Ideally matches the reference speed for grid plots
-ax.quiverkey(
-    sm,
-    0,
-    radius,
-    reference_speed,
-    label=f'{reference_speed} m/s',
-    coordinates='data',
-)
-ax.axis('off')
-fig.colorbar(sm, cax=cax, label='Altitude (km)')
-cax.axhline(
-    shot.elev_m / 1000,  # Horizontal line at shot elevation
-    linestyle='--',
-    color='black',
-    linewidth=plt.rcParams['axes.linewidth'],
-)
-cax.set_ylim(bottom=0)
-cax.set_facecolor(under_color)
-ax_pos = ax.get_position()
-cax_pos = cax.get_position()
-cbar_height = ax_pos.height * 0.5
-cax.set_position(
-    [
-        ax_pos.x1,
-        ax_pos.y0 + (ax_pos.height - cbar_height) / 2,
-        cax_pos.width,
-        cbar_height,
-    ]
-)
-ax.set_title(title_str, pad=30)
-
-fig.show()
-
-if SAVE:
-    fig.savefig(
-        NODAL_WORKING_DIR / 'figures' / 'era5_profiles' / f'shot_{SHOT}.png',
-        dpi=300,
-        bbox_inches='tight',
+    ax.axhline(
+        shot.elev_m / 1000,  # Horizontal line at shot elevation
+        linestyle='--',
+        color='black',
+        linewidth=plt.rcParams['axes.linewidth'],
+        zorder=-5,
+        label='Shot elevation',
     )
+    ax.set_xlabel('Wind speed (m/s)')
+    ax.set_ylabel('Altitude (km)')
+    title_str = (
+        rf'$\bf{{Shot~{shot.name}}}$'
+        + '\nModel time: '
+        + UTCDateTime(str(ds_plot.time.values)).strftime('%Y-%m-%d %H:%M')
+        + f'\nProfile location: ({ds_plot.latitude:.2f}°, {ds_plot.longitude:.2f}°)'.replace(
+            '-', '–'
+        )
+    )
+    ax.set_title(title_str)
+    ax.set_ylim(0, MAX_ALT)
+    ax.legend()
+    fig.show()
+
+    #%% Plot the profile (map view) [ABOVE CELL MUST BE RUN FIRST]
+
+    fig, (ax, cax) = plt.subplots(
+        ncols=2, gridspec_kw=dict(width_ratios=[40, 1]), figsize=(8, 8)
+    )
+
+    # Form colormap
+    cmap = cc.m_rainbow_r.copy()
+    under_color = 'tab:gray'
+    cmap.set_under(under_color)
+
+    sm = ax.quiver(
+        np.zeros(ds_plot.alt_km.size),
+        np.zeros(ds_plot.alt_km.size),
+        ds_plot.u,
+        ds_plot.v,
+        ds_plot.alt_km,  # We're coloring the arrows by altitude
+        cmap=cmap,
+        angles='xy',
+        scale_units='xy',
+        scale=1,
+        width=0.005,
+        clim=(shot.elev_m / 1000, MAX_ALT),  # Start colormap at shot elevation
+    )
+    radius = 30  # Hard-coded for the speeds we see here!
+    ax.set_xlim(-radius, radius)
+    ax.set_ylim(-radius, radius)
+    ax.set_aspect('equal')
+    reference_speed = 10  # [m/s]  # Ideally matches the reference speed for grid plots
+    ax.quiverkey(
+        sm,
+        0,
+        radius,
+        reference_speed,
+        label=f'{reference_speed} m/s',
+        coordinates='data',
+    )
+    ax.axis('off')
+    fig.colorbar(sm, cax=cax, label='Altitude (km)')
+    cax.axhline(
+        shot.elev_m / 1000,  # Horizontal line at shot elevation
+        linestyle='--',
+        color='black',
+        linewidth=plt.rcParams['axes.linewidth'],
+    )
+    cax.set_ylim(bottom=0)
+    cax.set_facecolor(under_color)
+    ax_pos = ax.get_position()
+    cax_pos = cax.get_position()
+    cbar_height = ax_pos.height * 0.5
+    cax.set_position(
+        [
+            ax_pos.x1,
+            ax_pos.y0 + (ax_pos.height - cbar_height) / 2,
+            cax_pos.width,
+            cbar_height,
+        ]
+    )
+    ax.set_title(title_str, pad=30)
+
+    fig.show()
+
+    if SAVE:
+        fig.savefig(
+            NODAL_WORKING_DIR / 'figures' / 'era5_profiles' / f'shot_{SHOT}.png',
+            dpi=300,
+            bbox_inches='tight',
+        )
