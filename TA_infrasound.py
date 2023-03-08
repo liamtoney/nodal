ba40358d152a6f51568d669122ff73997fb731f9
Liam Toney <ldtoney@alaska.edu>
2023-03-08 12:13:20 -0900

    Updates for a slicker waveform summary plot and preliminary spectral analysis

--- TA_infrasound.py
+++ TA_infrasound.py
@@ -78,56 +78,171 @@ ax.set_title(
 fig.tight_layout()
 fig.show()
 
-#%% Plot waveform for a shot
+#%% Flatten into DataFrame
 
-SHOT = 'Y6'
-STATION = 'E04D'
+import pandas as pd
 
+shot_matrix, station_names_matrix = np.meshgrid(df.index, station_names)
+
+ta_df = pd.DataFrame(
+    dict(
+        shot=shot_matrix.flatten(),
+        station=station_names_matrix.flatten(),
+        dist_km=dist_all.flatten(),
+    )
+)
+
+#%% Unified record section
+
+import matplotlib.patches as patches
+
+MAX_DIST = 40  # [km]
+
+# [Hz]
 FREQMIN = 0.5
 FREQMAX = 5
 
-CELERITY = 340  # [m/s] For marking predicted arrival
+# [m/s]
+MIN_C = 320
+MAX_C = 345
 
-# Download waveform
-st = client.get_waveforms(
-    network='*',
-    station=STATION,
-    location='*',
-    channel=CHANNEL,
-    starttime=df.loc[SHOT].time - 100,  # Extra for tapering
-    endtime=df.loc[SHOT].time + 500,  # Covers spatial extent
-    attach_response=True,
+fig, ax = plt.subplots()
+
+for _, row in ta_df[ta_df.dist_km < MAX_DIST].sort_values(by='dist_km').iterrows():
+
+    print(f'{row.shot}–{row.station} ({row.dist_km:.2f} km)')
+
+    # Download waveform
+    st = client.get_waveforms(
+        network='*',
+        station=row.station,
+        location='*',
+        channel=CHANNEL,
+        starttime=df.loc[row.shot].time
+        + row.dist_km / (MAX_C / 1000)
+        - 10,  # Extra for tapering
+        endtime=df.loc[row.shot].time
+        + row.dist_km / (MIN_C / 1000)
+        + 10,  # Covers spatial extent
+        attach_response=True,
+    )
+    assert st.count() == 1
+
+    # Process waveform
+    st.detrend('linear')
+    st.remove_response()
+    st.taper(0.05)
+    st.filter('bandpass', freqmin=FREQMIN, freqmax=FREQMAX, zerophase=True)
+    st.normalize()
+
+    t = st[0].times(reftime=df.loc[row.shot].time)
+    ax.plot(t, st[0].data + row.dist_km, lw=0.5, color='black', solid_capstyle='round')
+    ax.text(
+        t[-1],
+        row.dist_km,
+        rf' $\bf{{{row.shot}}}$–{row.station}',
+        ha='left',
+        va='center',
+    )
+
+# ax.set_xlim(left=0)
+# ax.set_ylim(bottom=0)
+
+# Hard-coded for the 40 km max dist. case
+ax.set_xlim(0, 140)
+ax.set_ylim(0, 40)
+
+# Add celerity colors
+inc = 0.5  # [m/s]
+celerity_list = np.arange(MIN_C, MAX_C + inc, inc)
+cmap = plt.cm.get_cmap('cet_rainbow', len(celerity_list))
+colors = [cmap(i) for i in range(cmap.N)]
+xlim = np.array(ax.get_xlim())
+ylim = np.array(ax.get_ylim())
+for celerity, color in zip(celerity_list, colors):
+    ax.plot(
+        xlim,
+        xlim * celerity / 1000,
+        label=f'{celerity:g}',
+        color=color,
+        zorder=-2,
+        lw=0.5,
+    )
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)  # Scale y-axis to pre-plotting extent
+cel_alpha = 0.5
+rect = patches.Rectangle(
+    (xlim[0], ylim[0]),
+    np.diff(xlim)[0],
+    np.diff(ylim)[0],
+    edgecolor='none',
+    facecolor='white',
+    alpha=cel_alpha,
+    zorder=-1,
 )
+ax.add_patch(rect)
+mapper = plt.cm.ScalarMappable(cmap=cmap)
+mapper.set_array(celerity_list)
+cbar = fig.colorbar(mapper, ax=ax, label='Celerity (m/s)', aspect=30, pad=0.07)
+cbar.solids.set_alpha(1 - cel_alpha)
 
-# Calculate distance from shot to station
-coords = inventory.get_coordinates(st[0].id)
-dist_m = gps2dist_azimuth(
-    df.loc[SHOT].lat, df.loc[SHOT].lon, coords['latitude'], coords['longitude']
-)[0]
+ax.set_xlabel('Time from shot (s)')
+ax.set_ylabel('Distance from shot (km)')
 
-# Process waveform
-st.detrend('linear')
-st.remove_response()
-stf = st.copy()
-stf.taper(0.05)
-stf.filter('bandpass', freqmin=FREQMIN, freqmax=FREQMAX, zerophase=True)
+for side in 'top', 'right':
+    ax.spines[side].set_visible(False)
 
-# Make waveform plot w/ predicted arrival marked
-fig = plt.figure()
-stf.plot(fig=fig, method='full')
-ax = fig.axes[0]
-pred_arr_time = df.loc[SHOT].time + (dist_m / CELERITY)
-ax.axvline(
-    pred_arr_time.matplotlib_date,
-    color='red',
-    zorder=-5,
-    linewidth=2,
-    label=f'Predicted arrival time\n({CELERITY} m/s celerity)',
-)
-ax.set_title(f'Shot {SHOT}, {FREQMIN}–{FREQMAX} Hz bandpass')
-ax.set_ylabel('Pressure (Pa)')
-ax.set_xlim(df.loc[SHOT].time.matplotlib_date, (pred_arr_time + 30).matplotlib_date)
-ax.legend(loc='lower left')
 fig.show()
 
-# fig.savefig(f'/Users/ldtoney/Downloads/{SHOT}-{STATION}.png', dpi=300, bbox_inches='tight')
+#%% Investigate the nice arrivals
+
+from obspy import Stream
+from python_util import plot
+
+CELERITY = 343  # [m/s]
+
+PLOT_NOISE = False
+
+noise = -120 if PLOT_NOISE else 0  # Noise window offset to before signal arrival
+
+# These are the nice 5 arrivals
+SHOT_NODE_PAIRS = [
+    ['Y1', 'F05D'],
+    ['Y7', 'E04D'],
+    ['Y6', 'E04D'],
+    # ['X2', 'F04D'],  # NOISIER THAN OTHER ONES!
+    ['Y8', 'E04D'],
+]
+
+good_st = Stream()
+
+for (shot, station) in SHOT_NODE_PAIRS:
+
+    dist_km = ta_df[(ta_df.station == station) & (ta_df.shot == shot)].dist_km.values[0]
+
+    # Download waveform
+    st = client.get_waveforms(
+        network='*',
+        station=station,
+        location='*',
+        channel=CHANNEL,
+        starttime=df.loc[shot].time + dist_km / (CELERITY / 1000) - 60 + noise,
+        endtime=df.loc[shot].time + dist_km / (CELERITY / 1000) + 60 + 5 + noise,
+        attach_response=True,
+    )
+    assert st.count() == 1
+
+    st[0].stats.network = shot  # For record-keeping
+
+    # Process waveform
+    st.detrend('linear')
+    st.remove_response()
+
+    good_st += st[0]
+
+    # Plot
+    fig = plot.spec(st[0])
+    fig.axes[0].set_title(f'{shot}–{station} ({dist_km:.2f} km)')
+
+fig = plot.psd(good_st, win_dur=20, db_lim=(20, 80))
+plt.gcf().axes[0].set_xlim(0.5, 10)
