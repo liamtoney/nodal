1ac7c509a1175d890ebba8d6de80e2869374fbf1
Liam Toney <ldtoney@alaska.edu>
2023-03-22 09:30:35 -0800

    Add some spectrogram plotting code to node distance script

--- node_distance.py
+++ node_distance.py
@@ -63,3 +63,35 @@ for distance_to_plot in distance_sorted[:6]:
 fig1.subplots_adjust(top=0.8)
 fig1.legend(frameon=False, loc='upper center', ncol=2, numpoints=2)
 fig1.show()
+
+#%% Plot spectrograms a closely-spaced nodes for a given shot
+
+from python_util import plot
+
+from utils import get_waveforms_shot
+
+SHOT = 'X4'  # Shot to use data from
+
+st = get_waveforms_shot(SHOT, processed=True)
+
+for distance_to_plot in distance_sorted[:6]:
+    inds = np.where(distance == distance_to_plot)
+    print(f'{distance[inds][0]:.1f} m')
+    for station in codes[inds[1][0]], codes[inds[0][0]]:
+        plot.spec(
+            st.select(station=station)[0],
+            win_dur=1,
+            wf_lim=(-6, 6),
+            spec_lim=(5, 50),
+            db_lim=(-180, -120),
+        )
+
+#%% Plot specific spectrograms for shot X4
+
+from obspy import UTCDateTime
+
+st = get_waveforms_shot('X4', processed=True)
+for station in '4308', '4303':
+    tr = st.select(station=station)[0]
+    tr.trim(UTCDateTime(2014, 7, 25, 10, 10, 30), UTCDateTime(2014, 7, 25, 10, 11, 10))
+    plot.spec(tr, win_dur=0.5, wf_lim=(-3, 3), spec_lim=(10, 50), db_lim=(-160, -130))
