bccb0e89b75b328abf916476b59c5ec8417e7a00
Liam Toney <ldtoney@alaska.edu>
2023-03-23 10:26:43 -0800

    Combine into one plot and make more robust

--- fdprop/compare_waveforms.py
+++ fdprop/compare_waveforms.py
@@ -1,12 +1,11 @@
-# DRAFT CODE!
-
 import matplotlib.pyplot as plt
 import numpy as np
 from obspy import Stream, Trace
+from scipy.fft import fft, fftfreq
 
 from utils import NODAL_WORKING_DIR
 
-# Read in synthetics for several runs
+# Read in synthetics for several runs (takes a bit!)
 st_dict = {}
 for run, waveform_snapshot_interval, x_src in zip(
     ['11_shot_y5_hf', '13_shot_y5_hf_smaller_dt', '14_shot_y5_smaller_dx'],
@@ -36,50 +35,61 @@ for run, waveform_snapshot_interval, x_src in zip(
     st = st[::2]  # IMPORTANT: Keep only EVEN indices (0, 2, 4, ...)
     st_dict[run] = st
 
-#%% Plot time domain comparison
+#%% Plot time and frequency domain comparison between runs
 
 TARGET_DISTANCE = 4000  # [m]
 
-CELERITY = 343  # [m/s] For estimating first arrival time
+CELERITY = 338  # [m/s] For estimating first arrival time
 
-fig, ax = plt.subplots(figsize=(11, 4))
+fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(11, 7))
+
 for run, st in st_dict.items():
+
+    # Select the waveform at this distance
     ind = np.argwhere(np.array([tr.stats.x for tr in st]) == TARGET_DISTANCE)[0][0]
     tr = st[ind]
     assert tr.stats.x == TARGET_DISTANCE
+
+    # Obtain slice of waveform [ostensibly] containing the first peak
     arrival_time = tr.stats.starttime + tr.stats.x / CELERITY
-    # [s] Trim to time window to compute amp for normalization
-    tr_first_arrival = tr.copy().trim(arrival_time - 0.5, arrival_time + 0.5)
-    ax.plot(tr.times(), tr.data / tr_first_arrival.data.max(), label=run)
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Amplitude normalized to first peak')  # MANUALLY VERIFY THIS ON PLOT!
-ax.set_title(f'{TARGET_DISTANCE} m')
-ax.legend()
-fig.tight_layout()
-fig.show()
+    first_peak_win = arrival_time - 0.5, arrival_time + 0.3
+    tr_first_peak = tr.copy().trim(*first_peak_win)
 
-#%% Plot spectra comparison
+    # Plot waveforms normalized to the amplitude of their first peak
+    ax1.plot(tr.times(), tr.data / tr_first_peak.data.max())
 
-from scipy.fft import fft, fftfreq
-
-fig, ax = plt.subplots()
-for run, st in st_dict.items():
-    ind = np.argwhere(np.array([tr.stats.x for tr in st]) == TARGET_DISTANCE)[0][0]
-    tr = st[ind]
-    assert tr.stats.x == TARGET_DISTANCE
-    arrival_time = tr.stats.starttime + tr.stats.x / CELERITY
-    # [s] Trim to time window to compute amp for normalization
-    tr_first_arrival = tr.copy().trim(arrival_time - 0.5, arrival_time + 0.5)
-    tr_spec = tr.copy().trim(tr.stats.starttime + 10, tr.stats.starttime + 20)
-    yf = fft(tr_spec.data / tr_first_arrival.data.max())
+    # Compute and plot spectra (code adapted from Figure0_Source.py)
+    spec_win = arrival_time - 5, arrival_time + 10
+    tr_spec = tr.copy().trim(*spec_win)  # Avoid waveform "edges"
+    yf = fft(tr_spec.data / tr_first_peak.data.max())
     xf = fftfreq(len(tr_spec.times()), tr_spec.stats.delta)[: len(tr_spec.times()) // 2]
     amp = 2.0 / len(tr_spec.times()) * np.abs(yf[0 : len(tr_spec.times()) // 2])
-    ax.plot(xf, amp, label=run)
-ax.set_xlim(0, 20)
-ax.set_ylim(bottom=0)
-ax.set_xlabel('Frequency (Hz)')
-ax.set_ylabel('Normalized amplitude')
-ax.set_title(f'{TARGET_DISTANCE} m')
-ax.legend()
+    ax2.plot(xf, amp, label=run)
+
+    # Plot the time windows used for selecting first peak and waveform for spectrum
+    ax1.axvspan(
+        *[t - tr.stats.starttime for t in first_peak_win],
+        color='tab:brown',
+        alpha=0.1,
+        zorder=-1,
+        lw=0,
+    )
+    ax1.axvspan(
+        *[t - tr.stats.starttime for t in spec_win],
+        color='gray',
+        alpha=0.05,
+        zorder=-2,
+        lw=0,
+    )
+
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Amplitude\n(waveform normalized\nto first peak)')
+ax1.set_xlim([t - tr.stats.starttime for t in (spec_win[0] - 2, spec_win[1] + 2)])
+ax1.set_title(f'{TARGET_DISTANCE} m')
+ax2.set_xlim(0, 20)
+ax2.set_ylim(bottom=0)
+ax2.set_xlabel('Frequency (Hz)')
+ax2.set_ylabel('Amplitude\n(waveform normalized\nto first peak)')
+ax2.legend(frameon=False)
 fig.tight_layout()
 fig.show()
