8ffae649af8017bdf7923d998bf95eb00ce84406
Liam Toney <ldtoney@alaska.edu>
2023-04-08 14:44:50 -0800

    Better table formatting approach

--- make_latex_shot_table.py
+++ make_latex_shot_table.py
@@ -5,43 +5,47 @@ import pandas as pd
 
 from utils import NODAL_WORKING_DIR, get_shots
 
+# Get the original shot DataFrame
 df = get_shots()
 
-# Read in individual CSV files (one per shot)
+# Convert shot weights from pounds to kilograms
+nearest_kg = 50  # [kg]
+df['weight_kg'] = (nearest_kg * round((df.weight_lb / 2.2046) / nearest_kg)).astype(int)
+
+# Read in individual CSV files (one per shot) and add summarized values to DataFrame
 shot_data = []
 for shot in df.index:
     shot_data.append(
         pd.read_csv(NODAL_WORKING_DIR / 'shot_gather_measurements' / f'{shot}.csv')
     )
-# RMS in μm/s and distance in km
+# Medians: RMS in μm/s and distance in km
 df['pre_shot_rms'] = [data.pre_shot_rms.median() * 1e6 for data in shot_data]
 df['median_dist'] = [data.dist_m.median() / 1000 for data in shot_data]
 
-df = df.reset_index(level=0)  # Convert the index (which is shot name) to a column
 df = df.sort_values(by='time')  # Sort from earliest to latest in time
-
-
-# Convert pounds to kilograms and round
-nearest_kg = 50  # [kg]
-df.weight_lb = (nearest_kg * round((df.weight_lb / 2.20462) / nearest_kg)).astype(int)
+df = df.reset_index(level=0)  # Convert the index (which is shot name) to a column
 
 # Format column names for table heading (also determines the order AND which columns!)
 columns = dict(
-    shot=('', 'Shot'),
-    time=('UTC time', r'(MM-DD hh:mm:ss)'),
-    weight_lb=('', 'Weight (kg)'),  # Note we *are* actually doing the unit conversion!
-    pre_shot_rms=('Median RMS', r'velocity (\textmu m/s)'),
-    median_dist=('Median shot--node', 'distance (km)'),
-    gcas_on_nodes=('', 'GCAs?'),
+    shot='Shot',
+    time='UTC time\n(MM-DD hh:mm:ss)',
+    weight_kg='Weight (kg)',  # Note we *are* actually doing the unit conversion!
+    pre_shot_rms='Median RMS\nvelocity (\\textmu m/s)',
+    median_dist='Median shot--node\ndistance (km)',
+    gcas_on_nodes='GCAs?',
 )
-columns.update(
-    (k, tuple(rf'\textbf{{{vi}}}' for vi in v)) for k, v in columns.items()
-)  # Make header bold
 
-# Subset table
+# Re-format any newlines present in the column names so LaTeX can handle them properly
+# (also make the header bold!)
+max_lines = max([len(v.split('\n')) for v in columns.values()])
+for k in columns.keys():
+    lines = columns[k].split('\n')
+    lines_bold = [rf'\textbf{{{line}}}' for line in lines]
+    empty_lines_to_add = max_lines - len(lines_bold)
+    columns[k] = tuple(empty_lines_to_add * [''] + lines_bold)
+
+# Subset table and assign the [potentially multi-line] formatted column names
 df = df[columns.keys()]
-
-
 df.columns = pd.MultiIndex.from_tuples(columns.values())
 
 
@@ -54,7 +58,7 @@ def color_rows_by_gca_presence(row):
 
 
 # Style and output .tex file
-df.rename(columns=columns).style.hide(axis='index').format(
+df.style.hide(axis='index').format(
     {
         columns['time']: lambda t: '{}'.format(t.strftime('%m-%d %H:%M:%S')),
         columns['gcas_on_nodes']: lambda x: rf'\texttt{{{x}}}',
@@ -64,5 +68,5 @@ df.rename(columns=columns).style.hide(axis='index').format(
 ).apply(color_rows_by_gca_presence, axis='columns').to_latex(
     Path(os.environ['NODAL_FIGURE_DIR']).parent / 'shot_table.tex',
     hrules=True,
-    column_format='c' * len(df.columns),
+    column_format='c' * len(df.columns),  # Center every column
 )
