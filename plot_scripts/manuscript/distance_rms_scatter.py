7d03707b96f5c24e28b53cd0c1dc5ad461d594ca
Liam Toney <ldtoney@alaska.edu>
2023-04-21 16:47:57 -0800

    Increase export DPI

--- plot_scripts/manuscript/distance_rms_scatter.py
+++ plot_scripts/manuscript/distance_rms_scatter.py
@@ -145,4 +145,4 @@ fig.show()
 
 _ = subprocess.run(['open', os.environ['NODAL_FIGURE_DIR']])
 
-# fig.savefig(Path(os.environ['NODAL_FIGURE_DIR']).expanduser().resolve() / 'distance_rms_scatter.png', dpi=300)
+# fig.savefig(Path(os.environ['NODAL_FIGURE_DIR']).expanduser().resolve() / 'distance_rms_scatter.png', dpi=600)
