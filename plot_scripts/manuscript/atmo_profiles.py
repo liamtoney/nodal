3826c213cff5bdf3ece5537172d0f9315a28b6c2
Liam Toney <ldtoney@alaska.edu>
2023-05-02 12:57:50 -0800

    clip_on tweaks

--- plot_scripts/manuscript/atmo_profiles.py
+++ plot_scripts/manuscript/atmo_profiles.py
@@ -13,13 +13,14 @@ plt.rcParams.update({'font.size': FONT_SIZE})
 
 M_PER_KM = 1000  # [m/km]
 
+YLIM = (-0.5, 5)  # [km] Relative to shot z-position
+
 # Parameters for computing sound speed
 gamma = 1.4
 R = 8.31446  # [J/(mol*K)]
 M_air = 0.02896  # [kg/mol]
 
 # Make plot
-lw = 1.5
 fig, axs = plt.subplots(
     nrows=2, ncols=2, sharex='col', sharey=True, figsize=(3.47, 5.5)
 )
@@ -44,40 +45,47 @@ for ax_row, shot_name in zip(axs, ['Y5', 'X5']):
     # Adjust z to be relative to shot elevation
     z -= Z_SRC / M_PER_KM
 
+    # Clip to plotting extent
+    mask = (z >= YLIM[0]) & (z <= YLIM[1])
+    z_plot = np.append(z[mask], YLIM[1])
+    t_plot = np.append(t[mask], np.interp(YLIM[1], z, t))
+    u_plot = np.append(u[mask], np.interp(YLIM[1], z, u))
+    v_plot = np.append(v[mask], np.interp(YLIM[1], z, v))
+
     # Compute static [dry air] sound speed, see first equation for c_air in
     # https://en.wikipedia.org/wiki/Speed_of_sound#Speed_of_sound_in_ideal_gases_and_air
-    c = np.sqrt(gamma * (R / M_air) * t)  # [m/s]
+    c_plot = np.sqrt(gamma * (R / M_air) * t_plot)  # [m/s]
 
     # Compute dot product
     profile_start = (shot.lat, shot.lon)
     g = Geod(ellps='WGS84')
     az_start_to_end = g.inv(*profile_start[::-1], *PROFILE_END[::-1])[0]
-    waz = (90 - np.rad2deg(np.arctan2(v, u))) % 360  # [° from N]
-    wmag = np.sqrt(u**2 + v**2)
+    waz = (90 - np.rad2deg(np.arctan2(v_plot, u_plot))) % 360  # [° from N]
+    wmag = np.sqrt(u_plot**2 + v_plot**2)
     angle_diff = waz - az_start_to_end  # Sign does not matter!
     dot_product = wmag * np.cos(np.deg2rad(angle_diff))  # Treating baz as unit vector
 
     # Make c_eff and find surface value
-    c_eff = c + dot_product
-    c_eff_surface = np.interp(0, z, c_eff)  # Linear interpolation
-    c_eff_ratio = c_eff / c_eff_surface
+    c_eff_plot = c_plot + dot_product
+    c_eff_surface = np.interp(0, z_plot, c_eff_plot)  # Linear interpolation
 
     ax1, ax2 = ax_row
-    ax1.plot(u, z, lw=lw, color='tab:red', label='Eastward')
-    ax1.plot(v, z, lw=lw, color='tab:blue', label='Northward')
-    ax2.plot(c, z, lw=lw, color='tab:Gray', label='$c$')
-    ax2.plot(c_eff, z, lw=lw, color='black', label='$c_\mathrm{eff}$')
+    plot_kw = dict(lw=1.5, clip_on=False, solid_capstyle='round', dash_capstyle='round')
+    ax1.plot(u_plot, z_plot, color='tab:red', label='Eastward', **plot_kw)
+    ax1.plot(v_plot, z_plot, color='tab:blue', label='Northward', **plot_kw)
+    ax2.plot(c_plot, z_plot, color='black', alpha=0.4, label='$c$', **plot_kw)
+    ax2.plot(c_eff_plot, z_plot, color='black', label='$c_\mathrm{eff}$', **plot_kw)
     ax2.axvline(
         c_eff_surface,
-        lw=lw,
         color='black',
         linestyle=':',
         label='$c_\mathrm{eff}$ (surface)',
+        **plot_kw,
     )
     ax1.set_ylabel(f'Elevation relative to shot {shot_name} (km)')
     ax1.set_xlim(-15, 15)
     ax2.set_xlim(300, 350)
-    ax1.set_ylim(-0.5, 5)
+    ax1.set_ylim(YLIM)
     ax1.yaxis.set_major_locator(plt.MultipleLocator(2))
     ax1.yaxis.set_minor_locator(plt.MultipleLocator(1))
     ax1.xaxis.set_minor_locator(plt.MultipleLocator(5))
@@ -93,8 +101,9 @@ axs[0, 0].legend(
     loc='lower left',
     bbox_to_anchor=(0, 1),
     title=r'${\bf Wind\ component}$',
+    numpoints=2,
 )
-axs[0, 1].legend(frameon=False, loc='lower left', bbox_to_anchor=(0, 1))
+axs[0, 1].legend(frameon=False, loc='lower left', bbox_to_anchor=(0, 1), numpoints=2)
 axs[0, 1].tick_params(left=False, which='both')
 axs[1, 1].tick_params(left=False, which='both')
 for side in 'top', 'right':
