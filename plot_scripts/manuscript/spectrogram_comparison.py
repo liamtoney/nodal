41bfb99131f995a7b18c387892fcff59fe2ac189
Liam Toney <ldtoney@alaska.edu>
2023-05-03 10:54:12 -0800

    Add colorbar

--- plot_scripts/manuscript/spectrogram_comparison.py
+++ plot_scripts/manuscript/spectrogram_comparison.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import simplekml
 from matplotlib.colors import to_hex
+from matplotlib.transforms import Bbox
 from scipy.signal import spectrogram
 
-from utils import NODAL_WORKING_DIR, get_shots, get_stations, get_waveforms_shot
+from utils import (NODAL_WORKING_DIR, get_shots, get_stations,
+                   get_waveforms_shot)
 
 FONT_SIZE = 10  # [pt]
 plt.rcParams.update({'font.size': FONT_SIZE})
@@ -65,7 +67,7 @@ SPEC_LIM = (10, 50)  # [Hz]
 DB_LIM = (-160, -130)  # [dB]
 WIN_DUR = 0.5  # [s]
 TIME_LIM = (40, 55)  # [s]
-
+V_REF = 1  # [m/s]
 
 # Define function to plot waveform and spectrogram into existing axes
 def spec_existing(tr, spec_ax, wf_ax):
@@ -80,13 +82,11 @@ def spec_existing(tr, spec_ax, wf_ax):
     # Remove DC component to avoid divide by zero errors later
     f = f[1:]
     sxx = sxx[1:, :]
-    ref_val = 1  # [m/s]
-    # [dB rel. (ref_val <ref_val_unit>)^2 Hz^-1]
-    sxx_db = 10 * np.log10(sxx / (ref_val**2))
+    sxx_db = 10 * np.log10(sxx / (V_REF**2))
 
     # Plot waveform and spectrogram
     wf_ax.plot(tr.times(), tr.data * 1e6, 'black', linewidth=0.5)  # Converting to μm/s
-    spec_ax.pcolormesh(
+    pcm = spec_ax.pcolormesh(
         t,
         f,
         sxx_db,
@@ -120,8 +120,11 @@ def spec_existing(tr, spec_ax, wf_ax):
         fontsize=8,
     )
 
+    return pcm
+
 
 # Axis setup
+FIGSIZE = (7.17, 4)  # [in]
 spec_ratios = (6, 2, 0.5)  # `spec_ax` height, `wf_ax` height, spacer height
 spec_rows = 3
 spec_cols = 4
@@ -129,7 +132,7 @@ assert spec_rows * spec_cols == st.count()  # Basic check that we have enough su
 fig, axs = plt.subplots(
     ncols=spec_cols + 1,  # Extra column for `map_ax`
     nrows=(2 * spec_rows) + (spec_rows - 1),  # Two axes for each spec, plus spacer axes
-    figsize=(7.17, 4),
+    figsize=FIGSIZE,
     gridspec_kw=dict(
         height_ratios=(spec_ratios * spec_rows)[:-1], width_ratios=[1] * spec_cols + [2]
     ),
@@ -151,7 +154,7 @@ for spec_ind in np.arange(0, spec_rows * 3, 3)[:-1]:  # Share y-axes across rows
 spec_axs = axs[0::3, :-1].flatten()
 wf_axs = axs[1::3, :-1].flatten()
 for tr, spec_ax, wf_ax in zip(st, spec_axs, wf_axs):
-    spec_existing(tr, spec_ax, wf_ax)
+    pcm = spec_existing(tr, spec_ax, wf_ax)
 
 # Plot map, instructions for making `x4_spectrogram_stations.png` are:
 # (1) Open `x4_spectrogram_stations.kml` in Google Earth (keep default view)
@@ -191,6 +194,28 @@ for ax in axs[:-1, :].flatten():
 fig.tight_layout(pad=0.2, rect=(0, -0.06, 1, 1))
 fig.subplots_adjust(hspace=0, wspace=0.05)
 
+# Add colorbar
+cax = fig.add_subplot(gs[0, :2])
+cax_pos = cax.get_position()
+cax.set_position(
+    [
+        cax_pos.xmin,
+        cax_pos.ymax + (axs[1, 0].get_position().ymin - axs[3, 0].get_position().ymax),
+        cax_pos.width,
+        0.02,
+    ]
+)
+fig.colorbar(pcm, cax=cax, orientation='horizontal')
+cax.text(
+    1.02,
+    0.5,
+    f'Power (dB rel. {V_REF:g} [m/s]$^2$ Hz$^{{-1}}$)',
+    ha='left',
+    va='center',
+    transform=cax.transAxes,
+)
+cax.xaxis.tick_top()
+
 # Print image axis aspect ratio (informs how to crop the image)
 ratio = np.abs(np.diff(map_ax.get_ylim())[0] / np.diff(map_ax.get_xlim())[0])
 print(f'Map height / width = {ratio:.2f}')
@@ -199,4 +224,10 @@ fig.show()
 
 _ = subprocess.run(['open', os.environ['NODAL_FIGURE_DIR']])
 
-# fig.savefig(Path(os.environ['NODAL_FIGURE_DIR']).expanduser().resolve() / 'spectrogram_comparison.png', dpi=400)
+if False:
+    fig.savefig(
+        Path(os.environ['NODAL_FIGURE_DIR']).expanduser().resolve()
+        / 'spectrogram_comparison.png',
+        dpi=400,
+        bbox_inches=Bbox.from_bounds(0, 0, FIGSIZE[0], FIGSIZE[1] + 0.38),
+    )
